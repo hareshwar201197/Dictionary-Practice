61e3871778d5474caca71efd17c9ef49a6d1bb6f
hari <hareshwarmali20@gmail.com>
2023-09-10 10:15:14 +0530

    check dict is empty or not

---
+++ dict16.py
@@ -0,0 +1,8 @@
+
+# Write a Python program to check if a dictionary is empty or not
+
+dic1 = {'1': 'abhi', '2': 'raks', '3': 'pra'}
+if not dic1:
+    print('Dictionary is empty')
+else:
+    print('Dictionary is Not empty')
